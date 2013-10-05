9befd7d400446fae176412fb89dd092fc51c5062
Tobias Kräntzer <info@tobias-kraentzer.de>
2013-10-05 13:54:40 +0300

    Move the main "app" into an own file

---
+++ js/main.js
@@ -0,0 +1,22 @@
+window.onload = function() {
+
+        var gallery = document.createElement('div');
+        for (var i = 0; i < images.length; i++) {
+                var image = images[i];
+
+                var img = document.createElement('img');
+                gallery.appendChild(img);
+
+
+                img.src = image.url;
+                img.width = image.width;
+                img.height = image.height;
+
+                img.style.position = 'absolute';
+                img.style.top = '10px';
+                img.style.left = '20px';
+                // img.style.width = '200px';
+                // img.style.height =  '200px';
+        };
+        document.body.appendChild(gallery);
+};
