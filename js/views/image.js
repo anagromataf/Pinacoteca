8d686c1eeff1fb194ac6f125f325b6f169170790
Tobias Kräntzer <info@tobias-kraentzer.de>
2013-10-23 15:59:03 +0300

    Set size of image views and use the image as background.

--- js/views/image.js
+++ js/views/image.js
@@ -1,9 +1,33 @@
 
 App.ImagesView = Ember.CollectionView.extend({
     tagName: 'div',
-    itemViewClass: 'App.ImageView'
+    itemViewClass: 'App.ImageView',
+    
+    childViewsDidChange: function(views, start, removed, the) {
+      this.get('childViews').forEach(function(view) {
+        view.set('width', 100);
+        view.set('height', 100);
+      });
+    }
 });
 
 App.ImageView = Ember.View.extend({
-  templateName: "image"
+  templateName: "image",
+  
+  classNames: ['image'],
+  
+  width: 0,
+  height: 0,
+  
+  propertyDidChange: function (name) {
+    if (name == 'width' || name == 'height') {
+      this.$().css(name, this.get(name));
+    }
+  },
+  
+  didInsertElement: function(){
+    this.$().css('width', this.get('width'));
+    this.$().css('height', this.get('height'));
+    this.$().css('background-image', 'url(' + this.content.get('url') + ')');
+  }
 });
