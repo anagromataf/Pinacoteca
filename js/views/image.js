967aa4af53285e0f0f5b69c67c9fc6e38f278610
Tobias Kräntzer <info@tobias-kraentzer.de>
2013-10-24 20:23:45 +0300

    Add property for max aspect ratio

--- js/views/image.js
+++ js/views/image.js
@@ -1,124 +1,123 @@
-
 App.ImagesView = Ember.CollectionView.extend({
-    tagName: 'div',
-    itemViewClass: 'App.ImageView',
-    
-    childViewsDidChange: function(views, start, removed, the) {
-      this.updateGrid();
-    },
-    
-    didInsertElement: function(){
-      this.$().css('position', 'relative');
-      
-      var collection = this;
-      $(window).resize(function(){
-        collection.updateGrid();
-      });
+  tagName: 'div',
+  itemViewClass: 'App.ImageView',
+
+  margin: 10,
+  maxAspectRatio: 2.5,
+  
+  childViewsDidChange: function(views, start, removed, the) {
+    this.updateGrid();
+  },
+
+  didInsertElement: function() {
+    this.$().css('position', 'relative');
+
+    var collection = this;
+    $(window).resize(function() {
       collection.updateGrid();
-    },
-    
-    updateGrid: function() {
-      if (this.$()) {
-        
-        var margin = 10;
-        
-        var collectionWidth = this.$().width();
-        
-        var offsetY = margin;
-        
-        var currentRow = new Array();
-        
-        aspectRatioOfRow = function (currentRow) {
-          var rowAspectRatio = 0;
-          for (var x = 0; x < currentRow.length; x++) {
-            rowAspectRatio += currentRow[x].content.get('aspectRatio');
-          }
-          return rowAspectRatio;
-        };
-        
-        flushRow = function (rowAspectRatio) {
-          
-          var _width = collectionWidth - margin;
-          
-          var rowHeight = Math.round(_width / rowAspectRatio);
-          
-          var offsetX = margin;
-          
-          for (var z = 0; z < currentRow.length; z++) {   
-            var width = 0;
-            if (z == currentRow.length - 1) {
-              width = _width - offsetX;
-            } else {
-              var aspectRatio = currentRow[z].content.get('aspectRatio');
-              width = Math.round(aspectRatio * (_width - margin) / rowAspectRatio);
-            }
-            
-            currentRow[z].set('width', width);
-            currentRow[z].set('height', rowHeight);
-            
-            currentRow[z].set('offsetX', offsetX);
-            currentRow[z].set('offsetY', offsetY);
-           
-            offsetX += width + margin;
-           }
-           
-           currentRow = new Array();
-           
-           offsetY += rowHeight + margin;
-        };
-        
-        var views = this.get('childViews');
-        for (var i = 0; i < views.length; i++) {
-          var view = views[i];
-          
-          if (currentRow.length > 0) {            
-            var rowAspectRatio = aspectRatioOfRow(currentRow);
-            if (collectionWidth / (rowAspectRatio + view.content.get('aspectRatio')) < 180) {
-              flushRow(rowAspectRatio);
-            }
-          }
-          currentRow.push(view);
+    });
+    collection.updateGrid();
+  },
+
+  updateGrid: function() {
+    if (this.$()) {
+
+      var margin = this.get('margin');
+      var maxAspectRatio = this.get('maxAspectRatio');
+      var minAspectRatio = 1 / maxAspectRatio;
+      
+      var collectionWidth = this.$().width();
+      
+      var currentRow = new Array();
+      var offsetY = margin;
+      
+      aspectRatioOfRow = function(currentRow) {
+        var rowAspectRatio = 0;
+        for (var x = 0; x < currentRow.length; x++) {
+          rowAspectRatio += Math.max(minAspectRatio, Math.min(maxAspectRatio, currentRow[x].content.get('aspectRatio')));
         }
+        return rowAspectRatio;
+      };
+      
+      flushRow = function(rowAspectRatio) {
         
+        var _collectionWidth = collectionWidth;
+        var rowHeight = Math.round(_collectionWidth / rowAspectRatio);
+        var offsetX = margin;
+        
+        for (var z = 0; z < currentRow.length; z++) {
+          var width = 0;
+          if (z == currentRow.length - 1) {
+            width = _collectionWidth - offsetX;
+          } else {
+            var aspectRatio = Math.max(minAspectRatio, Math.min(maxAspectRatio, currentRow[z].content.get('aspectRatio')));
+            width = Math.round(aspectRatio * (_collectionWidth - margin) / rowAspectRatio);
+          }
+          width -= margin;
+          
+          currentRow[z].set('width', width);
+          currentRow[z].set('height', rowHeight);
+          currentRow[z].set('offsetX', offsetX);
+          currentRow[z].set('offsetY', offsetY);
+
+          offsetX += width + margin;
+        }
+
+        currentRow = new Array();
+        offsetY += rowHeight + margin;
+      };
+
+      var views = this.get('childViews');
+      for (var i = 0; i < views.length; i++) {
+        var view = views[i];
+
         if (currentRow.length > 0) {
           var rowAspectRatio = aspectRatioOfRow(currentRow);
-          flushRow(rowAspectRatio);
+          if (collectionWidth / (rowAspectRatio + Math.max(minAspectRatio, Math.min(maxAspectRatio, view.content.get('aspectRatio')))) < 180) {
+            flushRow(rowAspectRatio);
+          }
         }
-        
-        this.$().css('height', offsetY); 
+        currentRow.push(view);
       }
+
+      if (currentRow.length > 0) {
+        var rowAspectRatio = aspectRatioOfRow(currentRow);
+        flushRow(rowAspectRatio);
+      }
+
+      this.$().css('height', offsetY);
     }
+  }.observes('maxAspectRatio').observes('margin')
 });
 
 App.ImageView = Ember.View.extend({
   templateName: "image",
-  
+
   classNames: ['image'],
-  
+
   width: 0,
   height: 0,
   offsetX: 0,
   offsetY: 0,
-  
+
   widthObserver: function() {
     this.$().css('width', this.get('width'));
   }.observes('width'),
-  
+
   heightObserver: function() {
     this.$().css('height', this.get('height'));
   }.observes('height'),
-  
-  offsetXObserver: function () {
+
+  offsetXObserver: function() {
     this.$().css('left', this.get('offsetX'));
   }.observes('offsetX'),
-  
-  offsetYObserver: function () {
+
+  offsetYObserver: function() {
     this.$().css('top', this.get('offsetY'));
   }.observes('offsetY'),
-  
-  didInsertElement: function(){
+
+  didInsertElement: function() {
     this.$().css('position', 'absolute');
     this.$().css('background-image', 'url(' + this.content.get('url') + ')');
   }
 });
-
